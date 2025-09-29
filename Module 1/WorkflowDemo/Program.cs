6a43d53d38c6c2043bce17a843a397d55f230144
dkirich5 <dkirich23@gmail.com>
2025-09-28 21:52:44 -0700

    Initial
    Assignment 1 completion

---
+++ Module 1/WorkflowDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Author: Daniel Kirichenko
+Date: 9/28/2025
+Assignment: Programming Assignment 1
+*/
+
+/* Show lyrics to a song. */
+
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess the song by the lyrics");
+            Console.WriteLine("There's a lady who's sure, all that glitters is gold");
+            Console.WriteLine("And she's buying a ________ __ ______");
+            Console.WriteLine("When she gets there, she");
+            Console.WriteLine("knows, if the stores are all closed");
+
+        }
+    }
+}
